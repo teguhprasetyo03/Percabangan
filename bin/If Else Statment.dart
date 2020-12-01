e8936b5b7db62cb2ad8c7f8adac8050e98ec339f
Teguh Prasetyo <teguhprasetyo261211@gmail.com>
2020-12-01 20:41:17 +0700

    Commit Heula

---
+++ bin/If Else Statment.dart
@@ -0,0 +1,12 @@
+void main() {
+    var nilai = 70;
+
+    print('nilai = $nilai');
+
+    if(nilai == 80){
+      print('Nilai Anda Benar 70');
+    } else{
+      print('Nilai Anda Bukan 70');
+    }
+  print('selesai');
+}
