5e53d5603decad86058b4548d6733d3f19fe8135
Teguh Prasetyo <teguhprasetyo261211@gmail.com>
2020-12-08 21:51:01 +0700

    menambahkan file if else if statment

---
+++ bin/If Else If Statment.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  // tutorial if else statment
+
+  var nilai = 60;
+  print('Awal dari Program');
+
+  if (nilai == 70) {
+    print('Anda mendapatkan nilai 70');
+  } else if (nilai == 80) {
+    print('Anda mendapatkan nilai 80');
+  } else {
+    print('Nilai Anda Bukan 70 atau 80');
+  }
+
+  print('Ini adalah akhir dari program');
+}
