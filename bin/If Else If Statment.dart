ec1d0e850e8ee666279cce1a5d716b200fbc8e84
Teguh Prasetyo <teguhprasetyo261211@gmail.com>
2020-12-14 20:56:17 +0700

    commit heula

---
+++ bin/If Else If Statment.dart
@@ -0,0 +1,27 @@
+void main(){
+  // Tutorial If Else If Statment
+
+  /*
+    if(ekspresi 1){
+      statment;
+    } else if ( eskpresi 2){
+      statment 2;
+    } else {
+      statment default;
+     }
+   */
+
+  var nilai = 80;
+  print("Ini adalah awal Dari Program");
+
+  if(nilai == 70){
+     print("Anda Mendapatkan Nilai 70");
+  } else if (nilai == 80){
+    print("Anda Mendapatkan Nilai 80");
+  }
+  else{
+    print("Nilai Anda Bukan 70 atau 80");
+  }
+
+  print("Ini Akhir Dari Program");
+}
---
+++ bin/If Else Statement.dart
@@ -0,0 +1,11 @@
+void main(){
+  var nilai = 70;
+
+  print('nilai = $nilai');
+
+  if(nilai == 70){
+    print('Nilai Anda Benar 70');
+  } else{
+    print('Nilai Anda Bukan 70');
+  }
+}
